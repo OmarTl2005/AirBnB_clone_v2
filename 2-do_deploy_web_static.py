d333c16cbd71ead01e40d9374af41362e9fb8061
OmarTl2005 <hiba41145@gmail.com>
2023-10-06 00:30:29 +0100

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['100.26.56.245', '54.144.149.135']
 
 
 def do_deploy(archive_path):
@@ -54,13 +54,13 @@ def do_deploy(archive_path):
     success = False
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        run("sudo mkdir -p {}".format(folder_path))
+        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("sudo rm -rf /tmp/{}".format(file_name))
+        run("sudo mv {}web_static/* {}".format(folder_path, folder_path))
+        run("sudo rm -rf {}web_static".format(folder_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(folder_path))
         print('New version deployed!')
         success = True
     except Exception:
